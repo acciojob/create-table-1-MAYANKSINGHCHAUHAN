cfb0bdbb9a57b816e8c07a99ca3d634aeae6b638
Mayank singh chauhan <113712051+MAYANKSINGHCHAUHAN@users.noreply.github.com>
2025-05-17 07:31:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function insert_Row() {
-  const app = require("./index.js");
-app.listen(3000, () => {
-  console.log('server started');
-});
-  
-  
+    // Get the table element by ID
+    let table = document.getElementById("sampleTable");
+
+    // Insert a new row at the top (index 0)
+    let newRow = table.insertRow(0);
+
+    // Insert two new cells into the new row
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+
+    // Set the content of the new cells
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
 }
